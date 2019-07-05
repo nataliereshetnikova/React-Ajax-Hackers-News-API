d02cd6279d97f24db0d91ebeb1d0ee6240ef2cec
nataliereshetnikova <ab1748@gmail.com>
2019-07-05 11:45:10 -0400

    Updates

---
+++ precache-manifest.a7f5b1e4996a7a533ce6ff46d21fb5d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3e6332d5967fe0f47af80b0f141255",
+    "url": "/React-Ajax-Hackers-News-API/index.html"
+  },
+  {
+    "revision": "cd9fa9bd5f5af5715e55",
+    "url": "/React-Ajax-Hackers-News-API/static/css/main.e09dc97d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/React-Ajax-Hackers-News-API/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cd9fa9bd5f5af5715e55",
+    "url": "/React-Ajax-Hackers-News-API/static/js/main.4c6b6e7b.chunk.js"
+  },
+  {
+    "revision": "c4ba53b55c43d13b41bf",
+    "url": "/React-Ajax-Hackers-News-API/static/js/runtime~main.d34bd331.js"
+  }
+]);
